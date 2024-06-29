c62c875508ca27b4dabc961612fca30b2aa364bc
Ramdhan <109734843+mus937@users.noreply.github.com>
2024-06-30 05:34:13 +0700

    Create Barang.java

---
+++ Barang.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+public class Barang {
+    private String nama;
+    private int harga;
+
+    public Barang(String nama, int harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+}
